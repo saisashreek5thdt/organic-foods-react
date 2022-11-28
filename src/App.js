86720cfb6a5fb68d5ffcdcfeca41ecd6bb0c6648
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2022-11-28 11:47:41 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div>Hi Organic Foods</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<App />);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
